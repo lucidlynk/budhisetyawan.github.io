1b00f3046be6cfe86ff56157c10895120a6c358f
lucidlynk <buset.iwan@gmail.com>
2022-07-17 20:44:13 +0700

    Update sidebar.php

--- app/Views/template/sidebar.php
+++ app/Views/template/sidebar.php
@@ -47,37 +47,8 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
             
-            <!-- Heading -->
-            <!--
-            <div class="sidebar-heading">
-                Interface
-            </div>
-            -->
-            
-            
 
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-address-card"></i>
-                    <span>KIS</span>
-                </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Kepesertaan:</h6>
-                        <a class="collapse-item" href="/kis">KIS APBD</a>
-                        <a class="collapse-item" href="/kis/apbn">KIS APBN</a>
-                        <a class="collapse-item" href="/kis/input">PESERTA BARU</a>
-                        <a class="collapse-item" href="/kis/usul">DATA USULAN KIS</a>
-                    </div>
-                </div>
-            </li>
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <hr class="sidebar-divider">
+            
             
             <!-- Heading -->
             <!--
@@ -106,6 +77,36 @@
                     </div>
                 </div>
             </li>
+            <hr class="sidebar-divider">
+            <!-- Heading -->
+            <!--
+            <div class="sidebar-heading">
+                Interface
+            </div>
+            -->
+            
+            
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-address-card"></i>
+                    <span>KIS</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Kepesertaan:</h6>
+                        <a class="collapse-item" href="/kis">KIS APBD</a>
+                        <a class="collapse-item" href="/kis/apbn">KIS APBN</a>
+                        <a class="collapse-item" href="/kis/input">PESERTA BARU</a>
+                        <a class="collapse-item" href="/kis/usul">DATA USULAN KIS</a>
+                    </div>
+                </div>
+            </li>
+            <!-- Divider -->
+            
 
             <!-- Logout -->
             <!-- <li class="nav-item">
