82d0cb6d8f092d9a9b5e5b998dbc64ee2c652767
lucidlynk <buset.iwan@gmail.com>
2022-07-17 20:42:01 +0700

    Update sidebar.php

--- app/Views/template/sidebar.php
+++ app/Views/template/sidebar.php
@@ -76,6 +76,37 @@
             </li>
             <!-- Divider -->
             <hr class="sidebar-divider">
+
+            <hr class="sidebar-divider">
+            
+            <!-- Heading -->
+            <!--
+            <div class="sidebar-heading">
+                Interface
+            </div>
+            -->
+            
+            
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilitiesMenu"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-address-card"></i>
+                    <span>Menu</span>
+                </a>
+                <div id="collapseUtilitiesMenu" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Kepesertaan:</h6>
+                        <a class="collapse-item" href="/menu">Headline</a>
+                        <a class="collapse-item" href="/kis/apbn">KIS APBN</a>
+                        <a class="collapse-item" href="/kis/input">PESERTA BARU</a>
+                        <a class="collapse-item" href="/kis/usul">DATA USULAN KIS</a>
+                    </div>
+                </div>
+            </li>
+
             <!-- Logout -->
             <!-- <li class="nav-item">
                 <a class="nav-link" href="<?= base_url('/ppks'); ?>">
