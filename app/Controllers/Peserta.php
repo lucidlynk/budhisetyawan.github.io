ed6306ee3d3f7f4e59f7b0ebc5f8b09b17a799e9
busetbudhi <bstdinsos@gmail.com>
2022-07-18 08:08:58 +0700

    18072022 0808am

--- app/Controllers/Peserta.php
+++ app/Controllers/Peserta.php
@@ -50,95 +50,17 @@ class Peserta extends BaseController
         return view('program/dtks',$data);
     }
 
-    public function import()
+    public function dtkssearch()
     {
-        if (!$this->validate([
-            'file_excel' => [
-                'rules' => 'uploaded[file_excel]|max_size[file_excel,10240]|ext_in[file_excel,xls,xlsx]',
-                'errors' => [
-                    'uploaded' => 'File Upload Masih Kosong',
-                    'max_size' => 'Max ukuran file 10 Mb',
-                    'ext_in' => 'File Harus dalam bentuk .xls atau .xlsx',
-                ]
-            ],'file' => [
-                'rules' => 'uploaded[file]|max_size[file,2048]|mime_in[file,application/pdf]',
-                'errors' => [
-                    'uploaded' => 'Lampiran File Upload Masih Kosong',
-                    'max_size' => 'Ukuran Lampiran File terlalu besar',
-                    'mime_in' => 'Lampiran yang ada pilih bukan pdf'
-                ]
-            ]
-        ])) {
-            session()->setFlashdata('error', $this->validator->listErrors());
-            return redirect()->back()->withInput();
-        }
-
-            //pdf
-            $filebaru= $this->request->getFile('file');
-         
-            //generate nama file random
-            $namaFile= $filebaru->getRandomName();
-            //upload gamabar
-            $filebaru->move('file',$namaFile);
-            
-            
-        $file = $this->request->getFile('file_excel');
-        $ext = $file->getClientExtension();
-
-        if($ext == 'xls'){
-            $render = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
-        }else{
-            $render = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-        }
-        $spreadsheet = $render->load($file);
-        $sheet= $spreadsheet->getActiveSheet()->toArray();
-
-        // $dataGagal = 0;
-        $dataBerhasil = 0;
-        foreach ($sheet as $x => $excel) {
-            $excel[7] = date('Y-m-d', strtotime($excel[7]));
-            //skip judul tabel
-            if($x==0){
-                continue;
-            }
-
-            //skip jika ada data yang sama
-            // $nik = $this->usulkisModel->cekdata($excel['3']);
-            // if (empty($nik)) {
-            //     $nik['nik']='';
-            // }
-            // if ($excel['3'] == $nik['nik']) {
-            //     $dataGagal++;         
-            //     continue;
-            // }
-            
-            $data = [
-                'noka' => $excel['1'],
-                'kk' => $excel['2'],
-                'nik' => $excel['3'],
-                'nama'=> $excel['4'],
-                'pisat' => $excel['5'],
-                'tmp_lahir' => $excel['6'],
-                'tgl_lahir' => $excel['7'],
-                'jk' => $excel['8'],
-                'status' => $excel['9'],
-                'alamat' => $excel['10'],
-                'kd_pos' => $excel['11'],
-                'kecamatan' => $excel['12'],
-                'desa' => $excel['13'],
-                'userid' => user()->id,
-                'berkas' => $namaFile,
-            ];
-            if($data['nik']==''){
-                continue;
-            }else{
-                $this->usulkisModel->save($data);
-                $dataBerhasil++;
-            }
-            
-        }
-        session()->setFlashdata('pesan',$dataBerhasil.' Data berhasil ditambahkan');
-        return redirect()->to('kis/input');
+        $id= $this->request->getVar('nama');
+        
+        $data=[
+            'tittle' => 'DTKS'
+        ];
+        $q = $this->db->query("SELECT * FROM dtks where nik={$id};");
+        $data['tampil'] = $q->getResultArray();
+        // dd($data);
+        return view('program/hasildtks',$data);
     }
 
     public function usul()
