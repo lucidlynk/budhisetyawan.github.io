0b219f593fca7d2c3180dac0a32992f92add90ce
busetbudhi <bstdinsos@gmail.com>
2022-08-06 12:03:21 +0700

    Update Dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -4,8 +4,19 @@ namespace App\Controllers;
 
 class Dashboard extends BaseController
 {
+    protected $db, $builder,$pmksModel;
+    public function __construct()
+    {
+        helper('form');
+        $this->db      = \Config\Database::connect();
+    }
     public function index()
     {
-        return view('dashboard/index');
+        $data=[
+            'apbd'=>$this->db->query("SELECT COUNT(noka) FROM apbd WHERE periode='JULI' AND tahun='2022'")->getRow()
+
+        ];
+        dd($data);
+        return view('dashboard/index',$data);
     }
 }
