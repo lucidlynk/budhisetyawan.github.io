3cf1eda3367e29ddb6e5b6b2ec15058797e2f58c
lucidlynk <buset.iwan@gmail.com>
2022-08-14 00:07:03 +0700

    14082022 1206am

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -185,7 +185,7 @@
                     <div class="row">
 
                         <!-- Area Chart -->
-                        <div class="col-xl-8 col-lg-7">
+                        <div class="col-xl-12 col-lg-12">
                             <div class="card shadow mb-4">
                                 <!-- Card Header - Dropdown -->
                                 <div
