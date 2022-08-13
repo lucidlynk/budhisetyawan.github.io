033da5be2188e4c75fbec93abbb5d80694653b7a
lucidlynk <buset.iwan@gmail.com>
2022-08-14 00:02:09 +0700

    coba 14082022 1202am

--- app/Views/template/head.php
+++ app/Views/template/head.php
@@ -21,82 +21,124 @@
 
     <!-- chart -->
 	<script>
-window.onload = function () {
+// window.onload = function () {
 
-var chart = new CanvasJS.Chart("chartContainer", {
-	exportEnabled: true,
-	animationEnabled: true,
-	title:{
-		text: ""
-	},
-	subtitles: [{
-		text: ""
-	}], 
-	axisX: {
-		title: "Kategori"
-	},
-	axisY: {
-		title: "",
-		titleFontColor: "#4F81BC",
-		lineColor: "#4F81BC",
-		labelFontColor: "#4F81BC",
-		tickColor: "#4F81BC",
-		includeZero: true
-	},
-	axisY2: {
-		title: "",
-		titleFontColor: "#C0504E",
-		lineColor: "#C0504E",
-		labelFontColor: "#C0504E",
-		tickColor: "#C0504E",
-		includeZero: true
-	},
-	toolTip: {
-		shared: true
-	},
-	legend: {
-		cursor: "pointer",
-		itemclick: toggleDataSeries
-	},
-	data: [{
-		type: "column",
-		name: "APBN",
-		showInLegend: true,      
-		yValueFormatString: "#.### Peserta",
-		dataPoints: [
-			{ label: "Januari",  y: 218987 },
-			{ label: "Februari", y: 220117 },
-			{ label: "Maret", y: 219271 },
-			{ label: "April",  y: 218362 },
-			{ label: "Mei",  y: 217365 }
-		]
-	},
-	{
-		type: "column",
-		name: "APBD",
-		axisYType: "secondary",
-		showInLegend: true,
-		yValueFormatString: "#.### Peserta",
-		dataPoints: [
-			{ label: "Januari", y: 240293 },
-			{ label: "Februari", y: 214866 },
-			{ label: "Maret", y: 182064 },
-			{ label: "April", y: 143119 },
-			{ label: "Mei", y: 142958 }
-		]
-	}]
-});
-chart.render();
+// var chart = new CanvasJS.Chart("chartContainer", {
+// 	exportEnabled: true,
+// 	animationEnabled: true,
+// 	title:{
+// 		text: ""
+// 	},
+// 	subtitles: [{
+// 		text: ""
+// 	}], 
+// 	axisX: {
+// 		title: "Kategori"
+// 	},
+// 	axisY: {
+// 		title: "",
+// 		titleFontColor: "#4F81BC",
+// 		lineColor: "#4F81BC",
+// 		labelFontColor: "#4F81BC",
+// 		tickColor: "#4F81BC",
+// 		includeZero: true
+// 	},
+// 	axisY2: {
+// 		title: "",
+// 		titleFontColor: "#C0504E",
+// 		lineColor: "#C0504E",
+// 		labelFontColor: "#C0504E",
+// 		tickColor: "#C0504E",
+// 		includeZero: true
+// 	},
+// 	toolTip: {
+// 		shared: true
+// 	},
+// 	legend: {
+// 		cursor: "pointer",
+// 		itemclick: toggleDataSeries
+// 	},
+// 	data: [{
+// 		type: "column",
+// 		name: "APBN",
+// 		showInLegend: true,      
+// 		yValueFormatString: "#.### Peserta",
+// 		dataPoints: [
+// 			{ label: "Januari",  y: 218987 },
+// 			{ label: "Februari", y: 220117 },
+// 			{ label: "Maret", y: 219271 },
+// 			{ label: "April",  y: 218362 },
+// 			{ label: "Mei",  y: 217365 }
+// 		]
+// 	},
+// 	{
+// 		type: "column",
+// 		name: "APBD",
+// 		axisYType: "secondary",
+// 		showInLegend: true,
+// 		yValueFormatString: "#.### Peserta",
+// 		dataPoints: [
+// 			{ label: "Januari", y: 240293 },
+// 			{ label: "Februari", y: 214866 },
+// 			{ label: "Maret", y: 182064 },
+// 			{ label: "April", y: 143119 },
+// 			{ label: "Mei", y: 142958 }
+// 		]
+// 	}]
+// });
+// chart.render();
 
-function toggleDataSeries(e) {
-	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-		e.dataSeries.visible = false;
-	} else {
-		e.dataSeries.visible = true;
-	}
-	e.chart.render();
-}
+// function toggleDataSeries(e) {
+// 	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+// 		e.dataSeries.visible = false;
+// 	} else {
+// 		e.dataSeries.visible = true;
+// 	}
+// 	e.chart.render();
+// }
 
-}
-</script>
+// }
+		window.onload = function () {
+
+		var chart = new CanvasJS.Chart("chartContainer", {
+		theme: "light1", // "light1", "light2", "dark1"
+		animationEnabled: true,
+		exportEnabled: false,
+		title: {
+			text: "Daftar PMKS"
+		},
+		axisX: {
+			margin: 0,
+			labelPlacement: "inside",
+			tickPlacement: "inside"
+		},
+		axisY2: {
+			title: "Views (in billion)",
+			titleFontSize: 14,
+			includeZero: true,
+			suffix: "jiwa"
+		},
+		data: [{
+			type: "bar",
+			axisYType: "secondary",
+			yValueFormatString: "#,###.## jiwa",
+			indexLabel: "{y}",
+			dataPoints: [
+			{ label: "Sugar - Maroon 5", y: 2000 },
+			{ label: "Sorry - Justin Bieber", y: 2000 },
+			{ label: "Johny Johny Yes Papa", y: 3000 },
+			{ label: "Gangnam Style", y: 4000 },
+			{ label: "Uptown Funk", y: 5000 },
+			{ label: "Masha and the Bear", y: 6000 },
+			{ label: "See You Again", y: 7000 },
+			{ label: "Shape of You", y: 8000 },
+			{ label: "Baby Shark Dance", y: 9000 },
+			{ label: "Despacito", y: 10000 }
+			]
+		}]
+		});
+		chart.render();
+		
+		}
+	</script>
 <!-- chart -->
