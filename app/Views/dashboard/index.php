d641a6bb4a0a261c362c3df5aa821c6927c3bbbb
lucidlynk <buset.iwan@gmail.com>
2022-08-14 00:16:49 +0700

    Update index.php

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -211,7 +211,7 @@
                                     <div class="chart-area">
                                         <!-- <canvas id="myAreaChart"></canvas> -->
                                         <!-- <canvas id="chartContainer"></canvas> -->
-                                        <div id="chartContainer" style="height: 500px; width: 100%;"></div>
+                                        <div id="chartContainer" style="height: 100%; width: 100%;"></div>
                                     </div>
                                 </div>
                             </div>
