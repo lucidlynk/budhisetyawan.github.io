eb3d5f5f3cdd2f634275850001e4362dd39ddbaf
busetbudhi <bstdinsos@gmail.com>
2022-08-15 11:28:13 +0700

    Update Dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -16,7 +16,8 @@ class Dashboard extends BaseController
     {
         $data=[
             'apbd'=>$this->db->query("SELECT FORMAT( COUNT(noka), 0) as jml FROM apbd WHERE periode='JULI' AND tahun='2022'")->getRow(),
-            'tampildata' => $this->pmksModel->getRekap()
+            'tampildata' => $this->pmksModel->getRekap(),
+            'tittle' => 'Dashboard'
 
         ];
         // dd($data);
