b22497431497249b7e7b53b5058bea2ef80ae4f7
lucidlynk <buset.iwan@gmail.com>
2022-08-21 15:34:52 +0700

    Update PmksModel.php

--- app/Models/PmksModel.php
+++ app/Models/PmksModel.php
@@ -91,7 +91,9 @@ class PmksModel extends Model
     public function getRekap()
     {
         $this->db      = \Config\Database::connect();
-        $q = $this->db->query("SELECT DISTINCT(nama_pmks),(SELECT COUNT(nama) FROM ppks WHERE ppks.id_pmks=pmks.id_pmks) AS jumlah FROM pmks LEFT JOIN ppks ON ppks.id_pmks=pmks.id_pmks ORDER BY jumlah DESC;");
+        $this->db->query('SET SESSION sql_mode = "TRADITIONAL"');
+        $q = $this->db->query("SELECT DISTINCT(nama_pmks),COUNT(nama)AS jumlah FROM pmks LEFT JOIN ppks ON ppks.id_pmks=pmks.id_pmks GROUP BY nama_pmks ORDER BY jumlah DESC;");
+        // $q = $this->db->query("SELECT DISTINCT(nama_pmks),(SELECT COUNT(nama) FROM ppks WHERE ppks.id_pmks=pmks.id_pmks) AS jumlah FROM pmks LEFT JOIN ppks ON ppks.id_pmks=pmks.id_pmks ORDER BY jumlah DESC;");
         $rekap = $q->getResultArray();
         return $rekap;
     }
